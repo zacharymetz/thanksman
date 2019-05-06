2f8f55a02dd32ae9ad4f5748fd5b2dfb94f1964a
Zachary Metz <zachary.metz@ucalgary.ca>
2019-05-06 13:03:29 -0600

    tracks changes made to objects

--- test.js
+++ test.js
@@ -3,10 +3,17 @@ const DatabaseAbstract = require('./abstract/database').DatabaseAbstract;
 
 async function test(){
     const db = await  DatabaseAbstract.create();
-    console.log(db);
-
-    console.log(db.User);
-    var currentUser = await db.User.where("userid = 5").firstOrDefault();
+    
+    //var currentUser = await db.User.where("userid = 1").firstOrDefault();
+   // console.log(currentUser);
+   // currentUser.password = "I have been changed lol";
+   // currentUser.email = "I haol";
+   // db.User.saveChanges()
+    var allUsers = await db.User.toList();
+   for(var i=0;i<allUsers.length;i++){
+       allUsers[i].password = "noice " + i.toString();
+   }
+   db.User.saveChanges()
 }
 
 test()
